e95e335151d5628f01be6553f81d4e045f9eed2d
= <satyanarayana.dhangeti@gmail.com>
2023-06-19 23:57:13 +0530

    Live API integration

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../utils/mockData";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const topRatedRestaurants = ()=>{
     //console.log(filterRestaurants);
 }
+
+
 const Body = ()=>{
+    useEffect(()=>{
+        fetchRestaurants();
+    },[])
     const [listOfRestaurants,setrestaurantList] = useState(restaurantList);
+        const fetchRestaurants = async()=>{
+            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.9890648&lng=82.2474648&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        //Optional chaining
+        setrestaurantList(json?.data?.cards[2]?.data?.data?.cards);
+    }
+    
 
     return (
         <div className="body">
