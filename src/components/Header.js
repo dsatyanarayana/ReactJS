158ef043c2c55317ee2139b3cdb1ed73a0eba948
= <satyanarayana.dhangeti@gmail.com>
2023-07-18 22:36:00 +0530

    Custom hook online and offline status

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState,useEffect } from "react";
 import {Link} from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 const Header = ()=>{    
     const [btnName,setBtnName] = useState("Login");
     useEffect(()=>{
         console.log("UseEffect Called");
     },[btnName])
-    
-
+    const onlineStatus = useOnlineStatus();
     return (
         <div className="res-header">
         <div className="res-header-logo">
@@ -15,6 +15,7 @@ const Header = ()=>{
         </div>
         <div className="res-header-navlist">
             <ul>
+                <li>{onlineStatus?"🟢":"🔴"}</li>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About Us</Link></li>
                 <li><Link to="/contact">Contact Us</Link></li>
---
+++ src/utils/useOnlineStatus.js
@@ -0,0 +1,16 @@
+const { useEffect,useState } = require("react");
+
+const useOnlineStatus = ()=>{
+    const [onlineStatus,setOnlineStatus] = useState(true);
+    useEffect(()=>{
+        window.addEventListener("online",()=>{
+            setOnlineStatus(true);
+        })
+        window.addEventListener("offline",()=>{
+            setOnlineStatus(false);
+        })
+
+    },[])
+    return onlineStatus;
+}
+export default useOnlineStatus;
